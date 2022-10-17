c4ca5df1638b89f251dd7eaac0f37afa9c5fd1bb
SharkyJunior <67413544+SharkyJunior@users.noreply.github.com>
2022-10-17 21:20:09 +0300

    v1.0.1 release

--- algorithms/rle.py
+++ algorithms/rle.py
@@ -10,60 +10,52 @@ def encode(path: str) -> None:
         for i in range(1, len(contents)):
             if contents[i-1] == contents[i]:
                 if noRepeatStr != '':
-                    noRepeatStr = contents[i-len(noRepeatStr)-1] + noRepeatStr
-                    noRepeatStr = noRepeatStr[:-1]
-                    final_string += toHelpByte(len(noRepeatStr), False)
-                    for char in noRepeatStr:
+                    noRepeatStr = contents[i-len(noRepeatStr)-1] + noRepeatStr[:-1]  # formatting sequence so it is written right
+                    final_string += toHelpByte(len(noRepeatStr), False)  # appending help byte (0.......)
+                    for char in noRepeatStr:  # adding sequence one by one
                         final_string += toByte(ord(char))
                     noRepeatStr = ''
                     length = 1
             elif contents[i-1] != contents[i]:
                 if length > 1 and noRepeatStr == '':
-                    final_string += toHelpByte(length, True)
-                    final_string += toByte(ord(contents[i-1]))
+                    final_string += toHelpByte(length, True) + toByte(ord(contents[i-1]))  # appending help byte (1.......) and symbol which was repeating
                     length = 0
                 if length == 1:
                     noRepeatStr += contents[i]
                     length = 0
             length += 1
         
+        
+        # handling last sequence which is not handled in the previous loop
         if length > 1:
-            final_string += toHelpByte(length, True)
-            final_string += toByte(ord(contents[i-1]))
-            length = 0
+            final_string += toHelpByte(length, True) + toByte(ord(contents[i-1]))   # appending help byte (1.......) and symbol which was repeating
         else:
-            noRepeatStr = contents[len(contents)-len(noRepeatStr)-1] + noRepeatStr
-            length = len(noRepeatStr)
-            final_string += toHelpByte(len(noRepeatStr), False)
-            for char in noRepeatStr:
+            noRepeatStr = contents[len(contents)-len(noRepeatStr)-1] + noRepeatStr  # formatting sequence so it is written right
+            length = len(noRepeatStr)  
+            final_string += toHelpByte(len(noRepeatStr), False)  # appending help byte (0.......)
+            for char in noRepeatStr:  # adding last sequence one by one
                 final_string += toByte(ord(char))
-            noRepeatStr = ''
-            length = 1
 
-        open(f"{path.replace('.txt', '')}_encoded.txt", 'w').write(final_string) 
+        open(f"{path.replace('.txt', '')}_encoded.txt", 'w').write(final_string)  # dumping all in the file
 
 
 def decode(path: str) -> None:
     with open(path, 'r') as file:
         contents = file.readline()
-        print(contents)
         decoded_string = ''
         
         counter = 0
         while counter < len(contents):
             helpByte = contents[counter:counter+8]
-            print('helpByte', helpByte)
-            if helpByte[0] == '1':
-                print('byte to add', contents[counter+8:counter+8+8], chr(int(contents[counter+8:counter+8+8], 2)), int(helpByte[1:], 2))
-                for i in range(int(helpByte[1:], 2)):
-                    decoded_string += chr(int(contents[counter+8:counter+8+8], 2))
+            if helpByte[0] == '1':  # if the following sequence is repeating symbols
+                for i in range(int(helpByte[1:], 2)):  # getting repeat count out of the help byte and adding that amount of repeating symbols
+                    decoded_string += chr(int(contents[counter+8:counter+8+8], 2))  # getting the byte right after the help byte 
                 counter += 16
-            else:
-                for i in range(counter, counter + 8 * (int(helpByte[1:], 2) + 1), 8):
-                    decoded_string += chr(int(contents[i:i+8], 2))
-                counter += 8 * (int(helpByte[1:], 2) + 1) 
+            else:  # if the following sequence is non-repeating symbols
+                for i in range(counter, counter + 8 * (int(helpByte[1:], 2) + 1), 8):  # going through bytes from (helpByte + 1) to (helpByte + unigueSymbolsLen + 1)
+                    decoded_string += chr(int(contents[i:i+8], 2))  # adding particular symbol (converted from byte)
+                counter += 8 * (int(helpByte[1:], 2) + 1)
                     
-        print(decoded_string)
         open(f"{path.replace('_encoded.txt', '')}_decoded.txt", 'w').write(decoded_string)
 
 
